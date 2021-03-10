02dc5f8d9348771956bea865cbecd2690f32e003
Danny Valencia <inge3neuromedia@gmail.com>
2021-03-10 15:34:53 -0500

    desarrollo pagina juegos 3 items

--- src/app/juegos/juegos.component.ts
+++ src/app/juegos/juegos.component.ts
@@ -8,13 +8,25 @@ import { JuegosService } from './../services/juegos.service';
   styleUrls: ['./juegos.component.css']
 })
 export class JuegosComponent implements OnInit {
+  item_tab_data: any[] = [];
+  info_tabla_de_2_campos: any[] = [];
+  info_cobros: any = {}; 
+  public activePillIndex:number = 0;
 
   constructor(private httpClient:HttpClient, private _juegosservice:JuegosService) { }
 
   ngOnInit(): void {
       this._juegosservice.getJuegos()
       .subscribe((res:any) => {
+        this.item_tab_data          = res.acf.item_tab;
+        this.info_tabla_de_2_campos = res.acf.item_tab.tabla_de_2_campos;
+        this.info_cobros            = res.acf.item_tab.seccion_cobros;
       });
   }
+
+  public selectPill(index:number) {
+    this.activePillIndex = index;
+    // do some other stuff if necessary...
+   }
   
 }
