e778a7153e56e92171f012a30aeb7da39b4b9f98
Nelson Zambrano <inge5neuromedia@gmail.com>
2021-09-23 15:25:33 -0500

    cambios gane v4

--- src/app/ruleta/ruleta.component.ts
+++ src/app/ruleta/ruleta.component.ts
@@ -17,15 +17,9 @@ export class RuletaComponent implements OnInit {
   activeRuleta(){
     $('#example1 ul').playSpin({
       time: 1000,
-      endNum: [1, 2, 7],
-      stopSeq: 'leftToRight',
-      onEnd: function() {
-          // ding.play();
-      },
-      onFinish: function() {
-          // sound.pause();
-      }
+      stopSeq: 'leftToRight'
   });
+    
   }
 
 }
