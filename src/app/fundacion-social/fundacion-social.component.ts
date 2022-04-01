06d49e40ce95bb15738a1c0d36197cfa2829f510
Nelson Zambrano <nelsonzambrano23@gmail.com>
2022-04-01 12:21:50 -0500

    cambios interna quienes somos fundacion social

--- src/app/fundacion-social/fundacion-social.component.ts
+++ src/app/fundacion-social/fundacion-social.component.ts
@@ -5,27 +5,32 @@ import { PagesService } from './../services/pages.service';
 @Component({
   selector: 'app-fundacion-social',
   templateUrl: './fundacion-social.component.html',
-  styleUrls: ['./fundacion-social.component.css']
+  styleUrls: ['./fundacion-social.component.css'],
 })
 export class FundacionSocialComponent implements OnInit {
   titulo: any = {};
   imagen_fondo: any;
   primer_grupo_repeater: any[] = [];
   segundo_grupo_texto_data: any = {};
+  banner_data: string;
 
   loader = true;
 
-  constructor(private httpClient:HttpClient, private _quienessomosfundacionservice:PagesService) { }
+  constructor(
+    private httpClient: HttpClient,
+    private _quienessomosfundacionservice: PagesService
+  ) {}
 
   ngOnInit(): void {
-    this._quienessomosfundacionservice.getQuienesSomosFundacion()
-      .subscribe((res:any) => {
+    this._quienessomosfundacionservice
+      .getQuienesSomosFundacion()
+      .subscribe((res: any) => {
         this.loader = false;
         this.titulo = res.acf.titulo_pagina;
         this.primer_grupo_repeater = res.acf.primer_grupo_texto;
         this.imagen_fondo = res.acf.imagen_fondo_derecha;
         this.segundo_grupo_texto_data = res.acf.segundo_grupo_texto;
+        this.banner_data = res.acf.banner_principal;
       });
   }
-
 }
