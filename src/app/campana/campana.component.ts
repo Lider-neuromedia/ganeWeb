296424f95839cb293c271e6a4f83d9b5aefe93f4
Nelson Zambrano <nelsonzambrano23@gmail.com>
2022-05-26 02:21:17 -0500

    commit 1

--- src/app/campana/campana.component.ts
+++ src/app/campana/campana.component.ts
@@ -1,38 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CampanaService } from './../services/campana.service';
 import Swal from 'sweetalert2';
+import { PagesService } from '../services/pages.service';
 
 @Component({
   selector: 'app-campana',
   templateUrl: './campana.component.html',
-  styleUrls: ['./campana.component.css']
+  styleUrls: ['./campana.component.css'],
 })
 export class CampanaComponent implements OnInit {
-
-  campana_data:any = {};
+  campana_data: any = {};
+  campanas_data: any[] = [];
   loader = true;
+  subtitulo_data: any;
 
-  constructor(private route: ActivatedRoute, private _campanaservice:CampanaService) { }
+  constructor(
+    private _router: Router,
+    private route: ActivatedRoute,
+    private _campanaservice: CampanaService,
+    private _campana: PagesService
+  ) {}
 
   ngOnInit(): void {
-    const slug = this.route.snapshot.paramMap.get('slug');
-    this._campanaservice.getCampana(slug)
-      .subscribe(res => {
+    this._campanaservice.getCampanas().subscribe((res: any) => {
+      this.loader = false;
+      this.campanas_data = res;
+    });
+    this._campana.getCampana().subscribe((res: any) => {
+      this.loader = false;
+      this.subtitulo_data = res.acf.subtitulo;
+    });
+    this.route.params.subscribe((param) => {
+      console.log(param.slug);
+      const slug = param.slug;
+      this._campanaservice.getCampana(slug).subscribe((res) => {
         this.loader = false;
         this.campana_data = res;
-        for(let campana of res){
+        for (let campana of res) {
           this.campana_data = campana;
         }
-      })
+      });
+    });
   }
-  errorCampana(){
+  verCampana(slug: string) {
+    this._router.navigate(['/campana', slug]);
+  }
+  errorCampana() {
     Swal.fire({
       icon: 'info',
       title: 'Oops...',
       text: 'La campaña seleccionada ya expiró',
-      confirmButtonColor: "#EAB73B",
-      confirmButtonText: "Cerrar",
+      confirmButtonColor: '#EAB73B',
+      confirmButtonText: 'Cerrar',
     });
-  };
+  }
 }
--- src/app/puntos-venta/puntos-venta.component.ts
+++ src/app/puntos-venta/puntos-venta.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 import { MapsService } from './../services/maps.service';
-import { NgForm, FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import {
+  NgForm,
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { Options } from 'select2';
 import { Select2OptionData } from 'select2';
 import Swal from 'sweetalert2';
@@ -19,7 +25,7 @@ const iconDefault = L.icon({
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   tooltipAnchor: [16, -28],
-  shadowSize: [41, 41]
+  shadowSize: [41, 41],
 });
 const iconUser = L.icon({
   iconRetinaUrl,
@@ -29,7 +35,7 @@ const iconUser = L.icon({
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   tooltipAnchor: [16, -28],
-  shadowSize: [41, 41]
+  shadowSize: [41, 41],
 });
 const iconGane = L.icon({
   iconRetinaUrl,
@@ -39,17 +45,16 @@ const iconGane = L.icon({
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   tooltipAnchor: [16, -28],
-  shadowSize: [41, 41]
+  shadowSize: [41, 41],
 });
 // L.Marker.prototype.options.icon = iconDefault;
 
 @Component({
   selector: 'app-puntos-venta',
   templateUrl: './puntos-venta.component.html',
-  styleUrls: ['./puntos-venta.component.css']
+  styleUrls: ['./puntos-venta.component.css'],
 })
 export class PuntosVentaComponent implements OnInit, AfterViewInit {
-
   private map;
 
   data: any = [];
@@ -57,78 +62,70 @@ export class PuntosVentaComponent implements OnInit, AfterViewInit {
   texto: any;
   dataMapsArr: any[] = [];
   loader = true;
-  dataFiltro: string[] = [];
+  dataFiltro: string[] = ['Apuestas', 'Giros', 'Recargas', 'BetPlay', 'Pagos'];
   monto: number;
   pnt_apertura: string;
   pnt_cierre: string;
-  filtro: string = "Giros";
+  filtro: string = 'Giros';
   lat: number;
   long: number;
-
+  srvGlobal: any;
 
   public formMaps: Array<Select2OptionData>;
   public options: Options;
   public formControl = new FormControl();
   marker: any;
 
-
-  constructor(private form: FormBuilder, private _maps: MapsService) { }
+  constructor(private form: FormBuilder, private _maps: MapsService) {}
 
   ngOnInit(): void {
     $('#filtro').select2({
       placeholder: 'Giros, Apuesta',
       language: {
-
         noResults: function () {
-
-          return "No hay resultado";
+          return 'No hay resultado';
         },
         searching: function () {
-
-          return "Buscando..";
-        }
-      }
+          return 'Buscando..';
+        },
+      },
     });
 
     $('#filtro').change(function () {
       filtraDatos($(this).val());
-    })
+    });
 
     const filtraDatos = (srv) => {
-      this.puntosMapa(srv);
-    }
+      this.srvGlobal = srv;
+      // this.puntosMapa(srv);
+    };
 
     $('#direcciones').select2({
       placeholder: 'Seleccione una dirección',
       language: {
-
         noResults: function () {
-
-          return "No hay resultado";
+          return 'No hay resultado';
         },
         searching: function () {
-
-          return "Buscando..";
-        }
-      }
+          return 'Buscando..';
+        },
+      },
     });
 
     $('#direcciones').change(function () {
-
       custom2(JSON.parse($(this).val()));
-    })
+    });
 
     const custom2 = (val: any) => {
       this.onSearch(val);
-    }
+    };
   }
 
   llenarFiltro() {
-    this.monto === 0? this.monto = null: this.monto;
-    if(this.monto === null){
+    this.monto === 0 ? (this.monto = null) : this.monto;
+    if (this.monto === null) {
       return;
     }
-    this.dataFiltro = ['Apuestas', 'Giros', 'Recargas', 'BetPlay', 'Pagos'];
   }
 
   onSearch(val: any) {
@@ -142,58 +139,79 @@ export class PuntosVentaComponent implements OnInit, AfterViewInit {
     this.map.flyTo([val.pnt_geox, val.pnt_geoy], 18);
   }
 
-
   ngAfterViewInit(): void {
     this.crearMapa();
   }
 
   crearMapa() {
-    navigator.geolocation.getCurrentPosition((pos) => {
-      this.lat = pos.coords.latitude;
-      this.long = pos.coords.longitude;
-    },
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        this.lat = pos.coords.latitude;
+        this.long = pos.coords.longitude;
+      },
       (err) => {
-        alert("No se encontró la ubicación");
-      });
-      this.map = L.map('map');
-      const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        alert('No se encontró la ubicación');
+      }
+    );
+    this.map = L.map('map');
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
         maxZoom: 18,
         minZoom: 3,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      });
-      tiles.addTo(this.map);
-      this.map.locate({enableHighAccuracy: true,
-                       setView: true,
-                       maxZoom: 15,
-                      timeout: 5000});
-      this.map.on('locationfound', e => {
-        const coords = [e.latlng.lat, e.latlng.lng];
-        L.marker(coords, {icon: iconUser}).bindPopup('<h6 style="color: #192D6D; font-size=21px;">Tu Ubicación</h6>').addTo(this.map);
-      })
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
+    tiles.addTo(this.map);
+    this.map.locate({
+      enableHighAccuracy: true,
+      setView: true,
+      maxZoom: 15,
+      timeout: 5000,
+    });
+    this.map.on('locationfound', (e) => {
+      const coords = [e.latlng.lat, e.latlng.lng];
+      L.marker(coords, { icon: iconUser })
+        .bindPopup(
+          '<h6 style="color: #192D6D; font-size=21px;">Tu Ubicación</h6>'
+        )
+        .addTo(this.map);
+    });
   }
 
   puntosMapa(srv: string) {
     Swal.fire('Reubicando puntos', 'Espere por favor', 'info');
     Swal.showLoading();
     this.data = {
-      "puntoGeox": 11,
-      "puntoGeoy": 19,
-      "srv_nombre": srv,
-      "monto": this.monto
-    }
-    this._maps.getLocations(this.data)
-      .subscribe((res: any) => {
-        let latRedondeo = this.lat.toFixed(2);
-        let longRedondeo = this.long.toFixed(2);
-        let i = 0;
-        for (let direccion of res.respuesta) {
-          if (direccion.pnt_geox !== null && direccion.pnt_geoy !== null && direccion.pnt_geox.toFixed(2) == latRedondeo && direccion.pnt_geoy.toFixed(2) == longRedondeo) {
-            this.marker = L.marker([direccion.pnt_geox, direccion.pnt_geoy], {icon: iconGane}).bindPopup('<h6 style="color: #192D6D; font-size=21px;">'+srv+'</h6>').addTo(this.map);
-            this.dataMaps.push(res.respuesta[i])
-          }
-          i++;
+      puntoGeox: 11,
+      puntoGeoy: 19,
+      srv_nombre: srv,
+      monto: this.monto,
+    };
+    this._maps.getLocations(this.data).subscribe((res: any) => {
+      let latRedondeo = this.lat.toFixed(2);
+      let longRedondeo = this.long.toFixed(2);
+      let i = 0;
+      for (let direccion of res.respuesta) {
+        if (
+          direccion.pnt_geox !== null &&
+          direccion.pnt_geoy !== null &&
+          direccion.pnt_geox.toFixed(2) == latRedondeo &&
+          direccion.pnt_geoy.toFixed(2) == longRedondeo
+        ) {
+          this.marker = L.marker([direccion.pnt_geox, direccion.pnt_geoy], {
+            icon: iconGane,
+          })
+            .bindPopup(
+              '<h6 style="color: #192D6D; font-size=21px;">' + srv + '</h6>'
+            )
+            .addTo(this.map);
+          this.dataMaps.push(res.respuesta[i]);
         }
-        Swal.close();
-      });
+        i++;
+      }
+      Swal.close();
+    });
   }
 }
